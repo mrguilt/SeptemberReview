b7035611fe85d91367a1196e8d350aaee605a02b
Charles Barilleaux <charles@mrguilt.com>
2025-09-22 14:35:54 -0400

    If Statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("September Review of Rust!");
+    println!("--------- ------ -- -----");
+
+    println!("\n## Control Flow");
+    println!("### If Statements");
+    let number=10;
+
+    println!("round 1: {number}");
+    if number<=5 {
+        println!("Under Five");
+    } else {
+        println!("Over 5");
+    }
+
+    let number=4;
+   println!("round 2: {number}");
+    if number<=5 {
+        println!("Under Five");
+    } else {
+        println!("Over 5");
+    }
 }
