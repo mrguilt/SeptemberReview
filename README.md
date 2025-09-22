4e913b20ef40e3cad2472f4bec7dcb3001257a98
Charles Barilleaux <charles@mrguilt.com>
2025-09-22 15:14:21 -0400

    Loops

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 I had a brief detour into Swift, which was fun, but I realized wasn't practical for me (I'll probably blog about it). But, it's been over a month since I played with Rust, and forgot a lot of it. This is going over the chunk of *The Rust Programming Language* I did to this point, to remember what I learned. 
 
 This will probably incorporate snippets of code I previously used.
+
+* `if` statements: what I expect initially
+* Mutable variables:
+    * I can do `let x=5` then `let x=6`--effectively, a new variable. I can't do `x=7`--`x` is *immutable*.
+    * If I do `let mut x=6`, then, it's *mutable*--I can subsequently do `x=7`.
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
     println!("\n## Control Flow");
     println!("### If Statements");
-    let number=10;
+    let mut number=10; //define variable as mutable. 
 
     println!("round 1: {number}");
     if number<=5 {
@@ -13,11 +13,23 @@ fn main() {
         println!("Over 5");
     }
 
-    let number=4;
+    number=4;
    println!("round 2: {number}");
     if number<=5 {
         println!("Under Five");
     } else {
         println!("Over 5");
     }
+    println!("\n### Loops"); //Mostly a straight copy-and-paste from an earlier program.
+    let mut counter=0;
+
+    let result=loop {
+        counter+=1;
+        print!("{counter}...");
+        if counter==10 {
+            break counter*2;
+        }
+    };
+
+    println!("\nThe result is {result}");
 }
