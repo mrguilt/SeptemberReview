3fe4729d262d02e960c72b3cf60324114df959cd
mrguilt <chbarr@bandman.local>
2026-05-10 10:13:50 -0400

    Checking git 3

---
+++ target/debug/deps/SeptemberReview-e0fef04ae89e4a60.d
@@ -0,0 +1,5 @@
+/Users/chbarr/programming/learn-rust/SeptemberReview/target/debug/deps/SeptemberReview-e0fef04ae89e4a60.d: src/main.rs
+
+/Users/chbarr/programming/learn-rust/SeptemberReview/target/debug/deps/SeptemberReview-e0fef04ae89e4a60: src/main.rs
+
+src/main.rs:
